077072057c15f734f7e1163b1d52066ed5bd6f00
Maj <159164690+majtobijakodric@users.noreply.github.com>
2025-11-27 22:33:32 +0100

    Added hardcoded users

--- js/loginAlert.js
+++ js/loginAlert.js
@@ -1,23 +1,38 @@
 // Select the <form> element on the page and add an event listener for the "submit" event
+const usernames = ["maj", "nik", "bacar", "maj@gmail.com", "nik@gmail.com"];
+const passwords = ["123", "123", "123", "123", "123"];
+
 document.querySelector("form").addEventListener("submit", function (e) {
 
     e.preventDefault(); // Normally the page reloads when you submit the form; this prevents it
 
     // Select the input field with the id "username"
-    const input = document.querySelector("#username");
+    const inputUser = document.querySelector("#username");
+    const inputPass = document.querySelector("#password");
 
     // Get the trimmed (no extra spaces) value from the input field
-    const value = input.value.trim();
+    const valueUser = inputUser.value.trim();
+    const valuePass = inputPass.value.trim();
 
-    // Create a new regular expression (RegExp) object using the pattern attribute from the input
-    // This allows us to test if the input matches the required format (email or username)
-    const pattern = new RegExp(input.pattern);
+    const matchedIndex = usernames.findIndex((name) => name === valueUser);
+    const isValidCredentials = matchedIndex !== -1 && passwords[matchedIndex] === valuePass;
 
-    Swal.fire({
-        background: "#201f1fff",
-        icon: "success",
-        title: "Login successful",
-        color: "white",
-        confirmButtonColor: "#1DB954"
-    });
+    if (isValidCredentials) {
+        Swal.fire({
+            background: "#201f1fff",
+            icon: "success",
+            title: "Login successful",
+            color: "white",
+            confirmButtonColor: "#1DB954"
+        });
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Login Failed",
+            text: "The credentials you entered are incorrect. Please try again.",
+            background: "#201f1fff",
+        });
+    }
+
+
 });
